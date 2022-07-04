52443ce23d254dcc4342eb731319f95c30099495
gjoe33 <seanjosephchege33@gmail.com>
2022-07-04 05:55:20 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * program that prints programing is like a puzzle
+ */
+
+/**
+ * main - program starts here
+ * description - program to print programming is like building a puzzle
+ */
+
+int main(void)
+{
+	put("Programming is like building a multilingual puzzle./n");
+	return (0);
+}
+
