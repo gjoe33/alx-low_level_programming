ab2f1916affecee25c93285310650accaa87bbc4
gjoe33 <seanjosephchege33@gmail.com>
2022-07-04 08:04:01 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ *main - program to print varoius types of computer
+ *description - to print various types of computer it is compiled and run
+ */
+
+void main(void)
+{
+	print("various types of computer it is compiled and run on .\n");
+	return (0);
+}
