9b750964106f530fdde61ad3c5604068e67e8f04
gjoe33 <seanjosephchege33@gmail.com>
2022-07-05 05:45:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int 0 - putchar.c(int n)
+{
+	int(print _putchar);
+	return (0);
+}
+/**
+ * main - entry point
+ * description - program to print _putchar
+ *return will be 0
+ */
+
+int main(void)
+{
+	putchar("char* put");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *description - program to print alpgabet in lowcase
+ * return = always 0.
+ */
+
+void main(void)
+{
+	putchar(int"print alphabet in lowcase.\n");
+	return (0);
+}
